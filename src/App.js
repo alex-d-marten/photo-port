a951978b865b4862501ea78754fcdeea0327fabd
amarten <alexdmarten@gmail.com>
2022-03-16 22:32:41 -0700

    rendering the contact form only when Contact is clicked

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import About from './components/About/index';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects'},
     { name: 'portraits', description: 'Portraits of people in my life' },
@@ -20,10 +22,18 @@ function App() {
       categories={categories}
       setCurrentCategory={setCurrentCategory}
       currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+          <Gallery currentCategory={currentCategory}></Gallery>
+          <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
